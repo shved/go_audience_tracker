5853d0da5e49cb0387586b8eb8e444ff4ae7d9a0
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2019-01-08 01:34:26 +0300

    set calculations

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_audience_tracker
+
+require github.com/deckarep/golang-set v1.7.1
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/deckarep/golang-set"
 	"log"
 	"net/http"
 	"net/url"
@@ -9,54 +10,67 @@ import (
 	"sync"
 )
 
-var customers []uint32
-var videos []uint32
+var customers = make(map[int][]int)
+var videos = make(map[int][]int)
 
 func pulse(w http.ResponseWriter, r *http.Request) {
 	customerID, videoID := parseQuery(r.URL.RawQuery)
-	newSession := session{VideoID: videoID, CustomerID: customerID}
 	mutex := &sync.Mutex{}
 
 	mutex.Lock()
 	customers[customerID] = append(customers[customerID], videoID)
-	videos[customerID] = append(videos[vidoeID], customerID)
+	videos[customerID] = append(videos[videoID], customerID)
 	mutex.Unlock()
 
-	log.Printf("pulse with customer %d and video %d", newSession.CustomerID, newSession.VideoID)
+	log.Printf("pulse with customer %d and video %d", customerID, videoID)
 }
 
-func parseQuery(rawQuery string) (customerID, videoID uint64) {
+func parseQuery(rawQuery string) (customerID, videoID int) {
 	values, _ := url.ParseQuery(rawQuery)
-	customerID, _ = strconv.ParseInt(values.Get("customer_id"))
-	videoID, _ = strconv.ParseInt(values.Get("video_id"))
+	customerID, _ = strconv.Atoi(values.Get("customer_id"))
+	videoID, _ = strconv.Atoi(values.Get("video_id"))
 	return
 }
 
-func videoStat(w http.ResponseWriter, r *http.Request) {
-	mutex := &sync.Mutex{}
-	videoID := parseIDFromURL(r.URL.Path)
-
-	mutex.Lock()
-	count := len(videos[videoID])
-	mutex.Unlock()
-
-	log.Println("video stat called: ", count)
-}
-
-func customerStat(w http.ResponseWriter, r *http.Request) {
+func customerCount(w http.ResponseWriter, r *http.Request) {
 	mutex := &sync.Mutex{}
 	customerID := parseIDFromURL(r.URL.Path)
 
 	mutex.Lock()
-	count := len(customers[customerID])
+	videoIDs := idsToInterfaceSlice(customers[customerID])
 	mutex.Unlock()
+
+	set := mapset.NewSetFromSlice(videoIDs)
+	count := set.Cardinality()
 
 	log.Println("customer stat called: ", count)
 }
 
-func parseIDFromURL(path string) (id uint64) {
+func videoCount(w http.ResponseWriter, r *http.Request) {
+	mutex := &sync.Mutex{}
+	videoID := parseIDFromURL(r.URL.Path)
+
+	mutex.Lock()
+	customerIDs := idsToInterfaceSlice(videos[videoID])
+	mutex.Unlock()
+
+	set := mapset.NewSetFromSlice(customerIDs)
+	count := set.Cardinality()
+
+	log.Println("video stat called: ", count)
+}
+
+func idsToInterfaceSlice(ids []int) []interface{} {
+	s := make([]interface{}, len(ids))
+	for i, v := range ids {
+		s[i] = interface{}(v)
+	}
+	return s
+}
+
+func parseIDFromURL(path string) (id int) {
 	stringSlice := strings.Split(path, "/")
-	id, _ = strconv.ParseInt(stringSlice[1])
+	id, _ = strconv.Atoi(stringSlice[1])
 	return
 }
 
@@ -64,8 +78,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/pulse", pulse)
-	mux.HandleFunc("/videos/", videoStat)
-	mux.HandleFunc("/customers/", customerStat)
+	mux.HandleFunc("/videos/", videoCount)
+	mux.HandleFunc("/customers/", customerCount)
 
 	http.ListenAndServe(":9292", mux)
 }
