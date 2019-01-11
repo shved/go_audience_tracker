361b5b592e3003d8a4d629e066f3a7353a490730
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2019-01-11 12:51:37 +0300

    dummy test update

--- dummy_test.rb
+++ dummy_test.rb
@@ -1,18 +1,18 @@
-`curl --get localhost:9292/pulse -d customer_id=1 -d video_id=1`
+`curl -v --get localhost:9292/pulse -d customer_id=1 -d video_id=1`
 sleep(0.3)
-`curl --get localhost:9292/pulse -d customer_id=1 -d video_id=2`
+`curl -v --get localhost:9292/pulse -d customer_id=1 -d video_id=2`
 sleep(0.3)
-`curl --get localhost:9292/pulse -d customer_id=1 -d video_id=3`
+`curl -v --get localhost:9292/pulse -d customer_id=1 -d video_id=3`
 sleep(0.3)
-`curl --get localhost:9292/pulse -d customer_id=2 -d video_id=1`
+`curl -v --get localhost:9292/pulse -d customer_id=2 -d video_id=1`
 sleep(0.3)
-`curl --get localhost:9292/customers/1`
+`curl -v --get localhost:9292/customers/1`
 sleep(0.3)
-`curl --get localhost:9292/videos/1`
+`curl -v --get localhost:9292/videos/1`
 sleep(0.3)
-`curl --get localhost:9292/videos/1`
+`curl -v --get localhost:9292/videos/1`
 sleep(0.3)
-`curl --get localhost:9292/videos/1`
+`curl -v --get localhost:9292/videos/1`
 sleep(0.3)
-`curl --get localhost:9292/videos/1`
+`curl -v --get localhost:9292/videos/1`
 sleep(0.3)
